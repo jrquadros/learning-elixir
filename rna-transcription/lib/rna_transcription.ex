85e07f48394264829c1bdf85042921b0dd15ef46
jrquadros <jrmquadros@gmail.com>
2020-04-07 00:31:16 -0300

    feat: rna_transcription finish

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -9,5 +9,12 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna
+    |> Enum.map(fn
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
+    end)
   end
 end
--- rna-transcription/test/rna_transcription_test.exs
+++ rna-transcription/test/rna_transcription_test.exs
@@ -6,22 +6,18 @@ defmodule RnaTranscriptionTest do
     assert RnaTranscription.to_rna('G') == 'C'
   end
 
-  @tag :pending
   test "transcribes cytosine to guanine" do
     assert RnaTranscription.to_rna('C') == 'G'
   end
 
-  @tag :pending
   test "transcribes thymidine to adenine" do
     assert RnaTranscription.to_rna('T') == 'A'
   end
 
-  @tag :pending
   test "transcribes adenine to uracil" do
     assert RnaTranscription.to_rna('A') == 'U'
   end
 
-  @tag :pending
   test "it transcribes all dna nucleotides to rna equivalents" do
     assert RnaTranscription.to_rna('ACGTGGTCTTAA') == 'UGCACCAGAAUU'
   end
